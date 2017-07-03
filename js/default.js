d6a8a55124e1bc9c3631483afc8e45d37f809863
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-07-03 19:02:15 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -111,6 +111,6 @@ function setCookie(c_name,value,expiredays){
                  
                  document.getElementById("bigimg").src='img/' + this.dataset.image+'.jpg';
                }
-           }
+           
            
            
