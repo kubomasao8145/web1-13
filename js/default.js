d28b9464664dc536e3ba7b86770cf80d4e9b4a35
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-07-03 18:59:31 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -27,7 +27,7 @@ if(filename === 'other.html'){
 }
 document.getElementById('form').select.onchange=function(){
    location.href=dicument.getElementById('form').select.value; 
-}
+
 
 
 var update= function(){
