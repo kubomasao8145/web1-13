9b33263c30534659074138038220a0655a3e7b75
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-07-03 18:59:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,7 +10,21 @@ var separate_time=function(time){
 
  var now = new Date();
 //var counter=separate_time(now);
+function getFileName(){
+   return window.location.href.split('/').pop();
+}
 
+var filename=getFileName();
+var opt;
+
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value="other.html"]');
+   
+}else{
+   opt=document.querySelector(option[value="index.html"]');
+}
+   opt.selected=true;
+}
 document.getElementById('form').select.onchange=function(){
    location.href=dicument.getElementById('form').select.value; 
 }
